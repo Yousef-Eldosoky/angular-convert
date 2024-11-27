61246d04cb6ac17196a65a9cbd1c3d42ccadb70a
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-11-27 07:33:22 +0200

    convert the home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { NavComponent } from "./shared/nav/nav.component";
+import { ItemsComponent } from "./shop/items/items.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, ItemsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
