1494dc78ef083a7c4e204e35b4ed01068eddbcfa
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-12-06 12:48:30 +0200

    add min hieght to product list

--- src/app/cart/product-list/product-list.component.ts
+++ src/app/cart/product-list/product-list.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ShopService } from '../../shop.service';
 import { ProductCardComponent } from "../product-card/product-card.component";
 import { ICartProduct } from '../../interface/i-cart-product';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, NgStyle],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   cart: ICartProduct[];
+  public screenHeight?: number;
+  
   constructor(private shopService: ShopService) {
     this.cart = this.shopService.getCart();
   }
@@ -20,5 +23,12 @@ export class ProductListComponent {
     this.shopService.cartSubject.subscribe((value) => {
       this.cart = value;
     });
+    this.screenHeight = window.innerHeight-176;
   }
+    
+    
+  @HostListener('window:resize', ['$event'])  
+  onResize() { 
+    this.screenHeight = window.innerHeight-176;
+  } 
 }
